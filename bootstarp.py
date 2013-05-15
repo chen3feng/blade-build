249decd380b9d413e2ec1b0bdc14eaf6c88eb55f
phongchen <phongchen@cf-VirtualBox.(none)>
2013-05-15 15:45:00 +0800

    More fixes

--- bootstarp.py
+++ bootstarp.py
@@ -7,7 +7,7 @@
 import sys
 import os.path
 
-zip_path = os.path.join(os.path.dirname(__file__), 'blade.zip')
+zip_path = os.path.abspath(os.path.join(os.path.dirname(__file__), 'blade.zip'))
 sys.path.append(zip_path)
 import blade_main
 
