76558dd9f4fe7fa5d7e2ba3a12af01e66a46eb8f
phongchen <phongchen@cf-VirtualBox.(none)>
2013-05-20 13:52:56 +0800

    Always use our copy of argparse because the bug of python 2.7.0: http:bugs.python.org/issue9340

--- bootstrap.py
+++ bootstrap.py
@@ -8,7 +8,7 @@ import sys
 import os.path
 
 zip_path = os.path.abspath(os.path.join(os.path.dirname(__file__), 'blade.zip'))
-sys.path.append(zip_path)
+sys.path.insert(0, zip_path)
 import blade_main
 
 blade_main.main(zip_path)
