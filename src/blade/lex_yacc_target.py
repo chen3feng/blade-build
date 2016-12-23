6eddb760d8f7f993cfe388bd71e2783f5dccb763
windleewt <liwenting_123@163.com>
2016-12-23 11:48:03 +0800

    lex_yacc_library

--- src/blade/lex_yacc_target.py
+++ src/blade/lex_yacc_target.py
@@ -4,11 +4,12 @@
 # Author: Feng Chen <phongchen@tencent.com>
 
 
-"""Define proto_library target
+"""Define lex_yacc_library target
 """
 
 
 import os
+import console
 import blade
 
 import build_rules
@@ -25,6 +26,9 @@ class LexYaccLibrary(CcTarget):
                  name,
                  srcs,
                  deps,
+                 warning,
+                 defs,
+                 incs,
                  recursive,
                  prefix,
                  blade,
@@ -34,21 +38,60 @@ class LexYaccLibrary(CcTarget):
         Init the cc lex yacc target
 
         """
-        if len(srcs) != 2:
-            raise Exception, ('"srcs" for lex_yacc_library should '
-                              'be a pair of (lex_source, yacc_source)')
+        if (len(srcs) != 2 or
+            (not (srcs[0].endswith('.l') or srcs[0].endswith('.ll'))) or
+            (not (srcs[1].endswith('.y') or srcs[1].endswith('.yy')))):
+            console.error_exit('%s: srcs for lex_yacc_library should be '
+                               'a pair of (lex_source, yacc_source)' % self.fullname)
+
         CcTarget.__init__(self,
                           name,
                           'lex_yacc_library',
                           srcs,
                           deps,
                           None,
-                          'yes',
-                          [], [], [], [], [], [],
+                          warning,
+                          defs,
+                          incs,
+                          [], [], [], [],
                           blade,
                           kwargs)
+
         self.data['recursive'] = recursive
         self.data['prefix'] = prefix
+        # Hardcode allow_undefined to resolve undefined reference
+        # error when building dynamic library
+        self.data['allow_undefined'] = True
+
+    def _setup_lex_yacc_flags(self):
+        """Set up lex/yacc flags according to the options. """
+        lex_flags, yacc_flags = [], []
+        yacc_flags.append('-d')
+        if self.data.get('recursive'):
+            lex_flags.append('-R')
+        prefix = self.data.get('prefix')
+        if prefix:
+            lex_flags.append('-P %s' % prefix)
+            yacc_flags.append('-p %s' % prefix)
+        env_name = self._env_name()
+        self._write_rule('%s.Replace(LEXFLAGS=%s)' % (env_name, lex_flags))
+        self._write_rule('%s.Replace(YACCFLAGS=%s)' % (env_name, yacc_flags))
+
+    def _generate_cc_source(self, var_name, src):
+        """Generate scons rules for cc source from lex/yacc source. """
+        env_name = self._env_name()
+        source = self._source_file_path(src)
+        target = self._target_file_path(src)
+        if src.endswith('.l') or src.endswith('.y'):
+            target = target[:-2]
+            rule = 'target = "%s", source = "%s"' % (target, source)
+            self._write_rule('%s = %s.CFile(%s)' % (var_name, env_name, rule))
+        elif src.endswith('.ll') or src.endswith('.yy'):
+            target = target[:-3]
+            rule = 'target = "%s", source = "%s"' % (target, source)
+            self._write_rule('%s = %s.CXXFile(%s)' % (var_name, env_name, rule))
+        else:
+            console.error_exit('%s: Unknown source %s' % (self.fullname, src))
 
     def scons_rules(self):
         """scons_rules.
@@ -57,65 +100,36 @@ class LexYaccLibrary(CcTarget):
 
         """
         self._prepare_to_generate_rule()
-
         env_name = self._env_name()
-        var_name = self._var_name()
-        lex_source_file = self._target_file_path(self.srcs[0])
-        lex_cc_file = '%s.cc' % lex_source_file
+        self._setup_lex_yacc_flags()
 
-        lex_flags = []
-        if self.data.get('recursive'):
-            lex_flags.append('-R')
-        prefix = self.data.get('prefix')
-        if prefix:
-            lex_flags.append('-P %s' % prefix)
-        self._write_rule(
-            'lex_%s = %s.CXXFile(LEXFLAGS=%s, target="%s", source="%s")' % (
-                var_name, env_name, lex_flags, lex_cc_file, lex_source_file))
-        yacc_source_file = os.path.join(self.build_path,
-                                        self.path,
-                                        self.srcs[1])
-        yacc_cc_file = '%s.cc' % yacc_source_file
-        yacc_hh_file = '%s.hh' % yacc_source_file
-
-        yacc_flags = []
-        if prefix:
-            yacc_flags.append('-p %s' % prefix)
-
-        self._write_rule(
-            'yacc_%s = %s.Yacc(YACCFLAGS=%s, target=["%s", "%s"], source="%s")' % (
-                var_name, env_name, yacc_flags,
-                yacc_cc_file, yacc_hh_file, yacc_source_file))
-        self._write_rule('%s.Depends(lex_%s, yacc_%s)' % (env_name,
-                                                          var_name, var_name))
+        lex_var_name = self._var_name('lex')
+        self._generate_cc_source(lex_var_name, self.srcs[0])
+        yacc_var_name = self._var_name('yacc')
+        self._generate_cc_source(yacc_var_name, self.srcs[1])
+        self._write_rule('%s.Depends(%s, %s)' % (
+                         env_name, lex_var_name, yacc_var_name))
 
         self._setup_cc_flags()
-
         self._write_rule('%s.Append(CPPFLAGS="-Wno-unused-function")' % env_name)
+
         obj_names = []
-        obj_name = '%s_object' % self._var_name_of(self.srcs[0] + '.cc')
+        obj_name = '%s_object' % self._var_name_of(self.srcs[0])
+        self._write_rule('%s = %s.SharedObject(%s)' % (obj_name, env_name, lex_var_name))
         obj_names.append(obj_name)
-        self._write_rule('%s = %s.SharedObject(target="%s" + top_env["OBJSUFFIX"], '
-                         'source="%s")' % (obj_name,
-                                             env_name,
-                                             lex_cc_file,
-                                             lex_cc_file))
-
-        obj_name = '%s_object' % self._var_name_of(self.srcs[1] + '.cc')
+        obj_name = '%s_object' % self._var_name_of(self.srcs[1])
+        self._write_rule('%s = %s.SharedObject(%s[0])' % (obj_name, env_name, yacc_var_name))
         obj_names.append(obj_name)
-        self._write_rule('%s = %s.SharedObject(target="%s" + top_env["OBJSUFFIX"], '
-                         'source="%s")' % (obj_name,
-                                             env_name,
-                                             yacc_cc_file,
-                                             yacc_cc_file))
-
-        self._write_rule('%s = [%s]' % (self._objs_name(), ','.join(obj_names)))
+        self._write_rule('%s = [%s]' % (self._objs_name(), ', '.join(obj_names)))
         self._cc_library()
 
 
 def lex_yacc_library(name,
                      srcs=[],
                      deps=[],
+                     warning='yes',
+                     defs=[],
+                     incs=[],
                      recursive=False,
                      prefix=None,
                      **kwargs):
@@ -123,6 +137,9 @@ def lex_yacc_library(name,
     target = LexYaccLibrary(name,
                             srcs,
                             deps,
+                            warning,
+                            defs,
+                            incs,
                             recursive,
                             prefix,
                             blade.blade,
