39a26c9dc3997d04335ba58293548ec6583d73e2
chen3feng <chen3feng@gmail.com>
2018-06-04 13:02:32 +0800

    Performance optimizations:
    1. optimize expand_deps generation
    2. optimize expors_incs
    3. optimize topological_sort and make it py3k compatiable
    4. optimize cpplint: don't call _get_opened_files if cpplint is disabled

--- src/blade/cu_targets.py
+++ src/blade/cu_targets.py
@@ -16,7 +16,7 @@ import blade
 import configparse
 
 import build_rules
-from blade_util import var_to_list
+from blade_util import var_to_list, stable_unique
 from cc_targets import CcTarget
 
 
@@ -86,17 +86,9 @@ class CuTarget(CcTarget):
             nvcc_flags += self._get_optimize_flags()
 
         # Incs
-        incs = self.data.get('incs', [])
-        new_incs_list = [os.path.join(self.path, inc) for inc in incs]
-        new_incs_list += self._export_incs_list()
-        # Remove duplicate items in incs list and keep the order
-        incs_list = []
-        for inc in new_incs_list:
-            new_inc = os.path.normpath(inc)
-            if new_inc not in incs_list:
-                incs_list.append(new_inc)
+        incs = self._get_incs_list()
 
-        return (nvcc_flags, incs_list)
+        return (nvcc_flags, incs)
 
 
     def _cu_objects_rules(self):
