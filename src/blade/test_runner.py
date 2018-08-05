dc80b44d141322ef2a4e1aab0b98b2b8b60a2310
CHEN Feng <chen3feng@gmail.com>
2018-08-05 10:08:21 -0400

    Add test progress

--- src/blade/test_runner.py
+++ src/blade/test_runner.py
@@ -403,7 +403,12 @@ class TestRunner(binary_runner.BinaryRunner):
         scheduler = TestScheduler(tests_run_list,
                                   concurrent_jobs,
                                   self.tests_run_map)
-        scheduler.schedule_jobs()
+        try:
+            scheduler.schedule_jobs()
+        except KeyboardInterrupt:
+            console.warning('KeyboardInterrupt, all tests stoped')
+            console.flush()
+            pass
 
         if self.coverage:
             self._generate_coverage_report()
