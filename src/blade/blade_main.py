41751d7911b627d06d1414c53dc57da72a914abe
CHEN Feng <chen3feng@gmail.com>
2019-03-29 21:50:29 -0400

    Improve progress bar in quiet mode

--- src/blade/blade_main.py
+++ src/blade/blade_main.py
@@ -312,6 +312,7 @@ def _run_ninja(cmd, options):
         return _run_native_builder(cmdstr)
     ninja_output = 'blade-bin/ninja_output.log'
     with open(ninja_output, 'w', buffering=1) as wf, open(ninja_output, 'r', buffering=1) as rf:
+        os.environ['NINJA_STATUS'] = '[%f/%t] '  # The progress depends on this format
         p = subprocess.Popen(cmdstr, shell=True, stdout=wf, stderr=subprocess.STDOUT)
         _show_progress(p, rf)
     return p.returncode
--- src/blade/console.py
+++ src/blade/console.py
@@ -162,7 +162,7 @@ def verbosity_ge(expected):
 ##############################################################################
 
 # Fit with the 80 columns terminal, leave some spaces for other parts such as the numbers.
-_PROGRESS_BAR_WIDTH = 65
+_PROGRESS_BAR_WIDTH = 60
 
 # TODO(chen3feng): Add lock
 _need_clear_line = False  # Whether the last output is progress bar
@@ -174,7 +174,8 @@ def _progress_bar(progress, current, total):
     [============================================================-----] 46/50
     """
     width = progress * _PROGRESS_BAR_WIDTH // 100
-    return '[%s%s] %s/%s' % ('=' * width, '-' * (_PROGRESS_BAR_WIDTH - width), current, total)
+    return '[%s%s] %s/%s %g%%' % ('=' * width, '-' * (_PROGRESS_BAR_WIDTH - width),
+                                  current, total, progress)
 
 
 def show_progress_bar(current, total):
