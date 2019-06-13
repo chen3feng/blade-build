chen3feng/blade-build

commit cca678cb810eabe4e2043fbf402ae02f42d4c571

Support python3.4+ (#534)
Change-Id: I47c3d6dc0cebe6733b9ff045549ad732161897f5

src/blade/toolchain.py
@@ -26,23 +26,25 @@ The toolchain function is defined as follows:
 
 """
 
+from __future__ import absolute_import
+
 import os
-import sys
 import shutil
 import socket
+import sys
+import tarfile
 import time
 import zipfile
-import tarfile
 
-import blade_util
-import console
-import fatjar
+from blade import blade_util
+from blade import console
+from blade import fatjar
 
 
 def generate_scm_entry(args):
     scm, revision, url, profile, compiler = args
-    f = open(scm, 'w')
-    f.write('''
+    with open(scm, 'w') as f:
+        f.write('''
 /* This file was generated by blade */
 extern "C" {
 namespace binary_version {
@@ -61,7 +63,6 @@ namespace binary_version {
        os.getenv('USER'),
        socket.gethostname(),
        compiler))
-    f.close()
 
 
 _PACKAGE_MANIFEST = 'MANIFEST.TXT'
@@ -83,11 +84,11 @@ def generate_zip_package(path, sources, destinations):
 
 
 _TAR_WRITE_MODES = {
-    'tar' : 'w',
-    'tar.gz' : 'w:gz',
-    'tgz' : 'w:gz',
-    'tar.bz2' : 'w:bz2',
-    'tbz' : 'w:bz2',
+    'tar': 'w',
+    'tar.gz': 'w:gz',
+    'tgz': 'w:gz',
+    'tar.bz2': 'w:bz2',
+    'tbz': 'w:bz2',
 }
 
 
@@ -133,11 +134,12 @@ def generate_securecc_object_entry(args):
 
 def generate_resource_index(targets, sources, name, path):
     header, source = targets
-    h, c = open(header, 'w'), open(source, 'w')
-    full_name = blade_util.regular_variable_name(os.path.join(path, name))
-    guard_name = 'BLADE_RESOURCE_%s_H_' % full_name.upper()
-    index_name = 'RESOURCE_INDEX_%s' % full_name
-    print >>h, '''// This file was automatically generated by blade
+    with open(header, 'w') as h, open('w', source) as c:
+        full_name = blade_util.regular_variable_name(os.path.join(path, name))
+        guard_name = 'BLADE_RESOURCE_%s_H_' % full_name.upper()
+        index_name = 'RESOURCE_INDEX_%s' % full_name
+
+        h.write('''// This file was automatically generated by blade
 #ifndef {0}
 #define {0}
 
@@ -153,24 +155,25 @@ struct BladeResourceEntry {{
     unsigned int size;
 }};
 #endif
-'''.format(guard_name)
-    print >>c, '''// This file was automatically generated by blade
+'''.format(guard_name))
+        c.write('''// This file was automatically generated by blade
 #include "{0}"
 
-const struct BladeResourceEntry {1}[] = {{'''.format(header, index_name)
+const struct BladeResourceEntry {1}[] = {{
+'''.format(header, index_name))
 
-    for s in sources:
-        entry_var = blade_util.regular_variable_name(s)
-        entry_name = os.path.relpath(s, path)
-        entry_size = os.path.getsize(s)
-        print >>h, '// %s' % entry_name
-        print >>h, 'extern const char RESOURCE_%s[%d];' % (entry_var, entry_size)
-        print >>h, 'extern const unsigned RESOURCE_%s_len;\n' % entry_var
-        print >>c, '    { "%s", RESOURCE_%s, %s },' % (entry_name, entry_var, entry_size)
+        for s in sources:
+            entry_var = blade_util.regular_variable_name(s)
+            entry_name = os.path.relpath(s, path)
+            entry_size = os.path.getsize(s)
+            h.write('// %s\n' % entry_name)
+            h.write('extern const char RESOURCE_%s[%d];\n' % (entry_var, entry_size))
+            h.write('extern const unsigned RESOURCE_%s_len;\n' % entry_var)
+            c.write('    { "%s", RESOURCE_%s, %s },\n' % (entry_name, entry_var, entry_size))
 
-    print >>c, '''}};
-const unsigned {0}_len = {1};'''.format(index_name, len(sources))
-    print >>h, '''// Resource index
+        c.write('''}};
+const unsigned {0}_len = {1};'''.format(index_name, len(sources)))
+        h.write('''// Resource index
 extern const struct BladeResourceEntry {0}[];
 extern const unsigned {0}_len;
 
@@ -178,9 +181,7 @@ extern const unsigned {0}_len;
 }}  // extern "C"
 #endif
 
-#endif  // {1}'''.format(index_name, guard_name)
-    h.close()
-    c.close()
+#endif  // {1}'''.format(index_name, guard_name))
 
 
 def generate_resource_index_entry(args):
@@ -238,9 +239,9 @@ def _get_all_test_class_names_in_jar(jar):
         basename = os.path.basename(name)
         # Exclude inner class and Test.class
         if (basename.endswith('Test.class') and
-            len(basename) > len('Test.class') and
-            not '$' in basename):
-            class_name = name.replace('/', '.')[:-6] # Remove .class suffix
+                len(basename) > len('Test.class') and
+                not '$' in basename):
+            class_name = name.replace('/', '.')[:-6]  # Remove .class suffix
             test_class_names.append(class_name)
     zip_file.close()
     return test_class_names
@@ -260,20 +261,18 @@ def _generate_java_test_coverage_flag(targetundertestpkg):
 
 
 def _generate_java_test(script, main_class, jars, args, targetundertestpkg):
-    f = open(script, 'w')
-    f.write(
-"""#!/bin/sh
+    with open(script, 'w') as f:
+        f.write("""#!/bin/sh
 # Auto generated wrapper shell script by blade
 
 if [ -n "$BLADE_COVERAGE" ]
 then
-  coverage_options="%s"
+    coverage_options="%s"
 fi
 
-exec java $coverage_options -classpath %s %s %s $@
-""" % (_generate_java_test_coverage_flag(targetundertestpkg), ':'.join(jars), main_class, args))
-    f.close()
-    os.chmod(script, 0755)
+exec java $coverage_options -classpath %s %s %s $@""" % (
+            _generate_java_test_coverage_flag(targetundertestpkg), ':'.join(jars), main_class, args))
+    os.chmod(script, 0o755)
 
 
 def generate_java_test_entry(args):
@@ -305,7 +304,7 @@ def generate_one_jar(onejar,
     zip_file = zipfile.ZipFile(bootjar, 'r')
     name_list = zip_file.namelist()
     for name in name_list:
-        if not name.lower().endswith('manifest.mf'): # Exclude manifest
+        if not name.lower().endswith('manifest.mf'):  # Exclude manifest
             onejar.writestr(name, zip_file.read(name))
             jar_path_set.add(name)
     zip_file.close()
@@ -332,7 +331,7 @@ def generate_one_jar(onejar,
     # Manifest
     # Note that the manifest file must end with a new line or carriage return
     onejar.writestr(os.path.join('META-INF', 'MANIFEST.MF'),
-                                 '''Manifest-Version: 1.0
+                    '''Manifest-Version: 1.0
 Main-Class: com.simontuffs.onejar.Boot
 One-Jar-Main-Class: %s
 
@@ -350,9 +349,8 @@ def generate_java_binary_entry(args):
     script, onejar = args
     basename = os.path.basename(onejar)
     fullpath = os.path.abspath(onejar)
-    f = open(script, 'w')
-    f.write(
-"""#!/bin/sh
+    with open(script, 'w') as f:
+        f.write("""#!/bin/sh
 # Auto generated wrapper shell script by blade
 
 jar=`dirname "$0"`/"%s"
@@ -362,8 +360,7 @@ fi
 
 exec java -jar "$jar" $@
 """ % (basename, fullpath))
-    f.close()
-    os.chmod(script, 0755)
+    os.chmod(script, 0o755)
 
 
 def generate_scala_test_entry(args):
@@ -372,34 +369,30 @@ def generate_scala_test_entry(args):
     test_class_names = _get_all_test_class_names_in_jar(jar)
     scala, java = os.path.abspath(scala), os.path.abspath(java)
     run_args = 'org.scalatest.run ' + ' '.join(test_class_names)
-    f = open(script, 'w')
-    f.write(
-"""#!/bin/sh
+    with open(script, 'w') as f:
+        text = """#!/bin/sh
 # Auto generated wrapper shell script by blade
 
 JAVACMD=%s exec %s -classpath %s %s $@
 
-""" % (java, scala, ':'.join(jars), run_args))
-    f.close()
-    os.chmod(script, 0755)
+""" % (java, scala, ':'.join(jars), run_args)
+        f.write(text)
+    os.chmod(script, 0o755)
 
 
 def generate_shell_test_entry(args):
     wrapper = args[0]
     scripts = args[1:]
-    f = open(wrapper, 'w')
-    f.write(
-"""#!/bin/sh
+    with open(wrapper, 'w') as f:
+        f.write("""#!/bin/sh
 # Auto generated wrapper shell script by blade
 
 set -e
 
 %s
 
-""" % '\n'.join(['. %s' % os.path.abspath(s) for s in scripts])
-)
-    f.close()
-    os.chmod(wrapper, 0755)
+""" % '\n'.join(['. %s' % os.path.abspath(s) for s in scripts]))
+    os.chmod(wrapper, 0o755)
 
 
 def generate_shell_testdata_entry(args):
@@ -409,10 +402,9 @@ def generate_shell_testdata_entry(args):
     middle = len(testdata) / 2
     sources = testdata[:middle]
     destinations = testdata[middle:]
-    f = open(path, 'w')
-    for i in range(middle):
-        f.write('%s %s\n' % (os.path.abspath(sources[i]), destinations[i]))
-    f.close()
+    with open(path, 'w') as f:
+        for i in range(middle):
+            f.write('%s %s\n' % (os.path.abspath(sources[i]), destinations[i]))
 
 
 def generate_python_library_entry(args):
@@ -423,12 +415,11 @@ def generate_python_library_entry(args):
     for py in args[2:]:
         digest = blade_util.md5sum_file(py)
         sources.append((py, digest))
-    f = open(pylib, 'w')
-    f.write(str({
-        'base_dir' : basedir,
-        'srcs' : sources
-    }))
-    f.close()
+    with open(pylib, 'w') as f:
+        f.write(str({
+            'base_dir': basedir,
+            'srcs': sources
+        }))
 
 
 def _update_init_py_dirs(arcname, dirs, dirs_with_init_py):
@@ -467,6 +458,7 @@ def _pybin_add_egg(pybin, libname):
         if name.endswith('.pyc'):
             return False
         return True
+
     _pybin_add_zip(pybin, libname, filter, None, None)
 
 
@@ -475,6 +467,7 @@ def _pybin_add_whl(pybin, libname, dirs, dirs_with_init_py):
         if '.dist-info/' in name:
             return False
         return True
+
     _pybin_add_zip(pybin, libname, filter, dirs, dirs_with_init_py)
 
 
@@ -500,18 +493,16 @@ def generate_python_binary(basedir, mainentry, path, args):
     pybin.writestr('__init__.py', '')
     pybin.close()
 
-    f = open(path, 'rb')
-    zip_content = f.read()
-    f.close()
+    with open(path, 'rb') as f:
+        zip_content = f.read()
     # Insert bootstrap before zip, it is also a valid zip file.
     # unzip will seek actually start until meet the zip magic number.
     bootstrap = ('#!/bin/sh\n\n'
                  'PYTHONPATH="$0:$PYTHONPATH" exec python -m "%s" "$@"\n') % mainentry
-    f = open(path, 'wb')
-    f.write(bootstrap)
-    f.write(zip_content)
-    f.close()
-    os.chmod(path, 0755)
+    with open(path, 'wb') as f:
+        f.write(bootstrap)
+        f.write(zip_content)
+    os.chmod(path, 0o755)
 
 
 def generate_python_binary_entry(args):
@@ -519,23 +510,22 @@ def generate_python_binary_entry(args):
 
 
 toolchains = {
-    'scm' : generate_scm_entry,
-    'package' : generate_package_entry,
-    'securecc_object' : generate_securecc_object_entry,
-    'resource_index' : generate_resource_index_entry,
-    'java_jar' : generate_java_jar_entry,
-    'java_resource' : generate_java_resource_entry,
-    'java_test' : generate_java_test_entry,
-    'java_fatjar' : generate_fat_jar_entry,
-    'java_onejar' : generate_one_jar_entry,
-    'java_binary' : generate_java_binary_entry,
-    'scala_test' : generate_scala_test_entry,
-    'shell_test' : generate_shell_test_entry,
-    'shell_testdata' : generate_shell_testdata_entry,
-    'python_library' : generate_python_library_entry,
-    'python_binary' : generate_python_binary_entry,
+    'scm': generate_scm_entry,
+    'package': generate_package_entry,
+    'securecc_object': generate_securecc_object_entry,
+    'resource_index': generate_resource_index_entry,
+    'java_jar': generate_java_jar_entry,
+    'java_resource': generate_java_resource_entry,
+    'java_test': generate_java_test_entry,
+    'java_fatjar': generate_fat_jar_entry,
+    'java_onejar': generate_one_jar_entry,
+    'java_binary': generate_java_binary_entry,
+    'scala_test': generate_scala_test_entry,
+    'shell_test': generate_shell_test_entry,
+    'shell_testdata': generate_shell_testdata_entry,
+    'python_library': generate_python_library_entry,
+    'python_binary': generate_python_binary_entry,
 }
-
 
 if __name__ == '__main__':
     name = sys.argv[1]

src/blade/__init__.py
@@ -1,0 +1,10 @@
+# See http://google-perftools.googlecode.com/svn/trunk/doc/heap_checker.html
+HEAP_CHECK_VALUES = set([
+    '',
+    'minimal',
+    'normal',
+    'strict',
+    'draconian',
+    'as-is',
+    'local',
+])

src/blade/binary_runner.py
@@ -20,13 +20,14 @@ import shutil
 import subprocess
 import sys
 
-from blade import console
 from blade import config
+from blade import console
 from blade.blade_util import environ_add_path
 
 
 class BinaryRunner(object):
     """BinaryRunner. """
+
     def __init__(self, targets, options, target_database):
         """Init method. """
         from blade import build_manager
@@ -67,7 +68,7 @@ class BinaryRunner(object):
         dest_norm = os.path.normpath(dest)
         if dest in dest_list:
             console.error_exit('Ambiguous testdata of %s: %s, exit...' % (
-                               target.fullname, dest))
+                target.fullname, dest))
         for item in dest_list:
             item_norm = os.path.normpath(item)
             if len(dest_norm) >= len(item_norm):
@@ -75,9 +76,9 @@ class BinaryRunner(object):
             else:
                 long_path, short_path = item_norm, dest_norm
             if (long_path.startswith(short_path) and
-                long_path[len(short_path)] == '/'):
+                    long_path[len(short_path)] == '/'):
                 console.error_exit('%s could not exist with %s in testdata of %s' % (
-                                   dest, item, target.fullname))
+                    dest, item, target.fullname))
 
     def _prepare_env(self, target):
         """Prepare the test environment. """
@@ -99,7 +100,7 @@ class BinaryRunner(object):
                                 '%s -> %s already exists\n'
                                 'skipped, should check duplicate prebuilt '
                                 'libraries'
-                        % (dst, src, dst, os.path.realpath(dst)))
+                                % (dst, src, dst, os.path.realpath(dst)))
                 continue
             os.symlink(src, dst)
 
@@ -196,7 +197,7 @@ class BinaryRunner(object):
         target = self.targets[target_key]
         if target.type not in self.run_list:
             console.error_exit('target %s:%s is not a target that could run' % (
-                       target_key[0], target_key[1]))
+                target_key[0], target_key[1]))
         run_env = self._prepare_env(target)
         cmd = [os.path.abspath(self._executable(target))] + self.options.args
         shell = target.data.get('run_in_shell', False)

src/blade/blade_main.py
@@ -71,7 +71,6 @@ from __future__ import absolute_import
 from __future__ import print_function
 
 import cProfile
-import datetime
 import errno
 import json
 import os
@@ -89,16 +88,13 @@ from blade import build_manager
 from blade import config
 from blade import console
 from blade import target
-
 from blade.blade_util import find_blade_root_dir, find_file_bottom_up
-from blade.blade_util import get_cwd
+from blade.blade_util import get_cwd, iteritems
 from blade.blade_util import lock_file, unlock_file
 from blade.command_args import CmdArguments
 
-
 # Run target
 _TARGETS = None
-
 
 _BLADE_ROOT_DIR = None
 _WORKING_DIR = None
@@ -148,7 +144,7 @@ def split_targets_into_scm_root(targets, working_dir):
 def _get_changed_files(targets, blade_root_dir, working_dir):
     scm_root_dirs = split_targets_into_scm_root(targets, working_dir)
     changed_files = set()
-    for scm_root, (scm, dirs) in scm_root_dirs.iteritems():
+    for scm_root, (scm, dirs) in iteritems(scm_root_dirs):
         try:
             os.chdir(scm_root)
             if scm == 'svn':
@@ -189,7 +185,7 @@ def _check_code_style(targets):
             else:
                 msg = 'Please fixing style warnings before submitting the code!'
             console.warning(msg)
-    except KeyboardInterrupt, e:
+    except KeyboardInterrupt as e:
         console.error(str(e))
         return 1
     return 0
@@ -287,9 +283,9 @@ def _ninja_build(options):
     cmd = ['ninja']
     cmd += native_builder_options(options)
     # if options.jobs:
-        # Unlike scons, ninja enable parallel building defaultly,
-        # so only set it when user specified it explicitly.
-        # cmd.append('-j%s' % options.jobs)
+    # Unlike scons, ninja enable parallel building defaultly,
+    # so only set it when user specified it explicitly.
+    # cmd.append('-j%s' % options.jobs)
     cmd.append('-j%s' % (options.jobs or build_manager.instance.parallel_jobs_num()))
     if options.keep_going:
         cmd.append('-k0')
@@ -458,8 +454,8 @@ def generate_scm(build_dir):
     path = os.path.join(build_dir, 'scm.json')
     with open(path, 'w') as f:
         json.dump({
-            'revision' : revision,
-            'url' : url,
+            'revision': revision,
+            'url': url,
         }, f)
 
 
@@ -543,7 +539,7 @@ def run_subcommand_profile(command, options, targets, blade_path, build_dir):
     console.output('Binary profile file `%s` is also generated, '
                    'you can use `gprof2dot` or `vprof` to convert it to graph, eg:' % pstats_file)
     console.output('  gprof2dot.py -f pstats --color-nodes-by-selftime %s'
-                 ' | dot -T pdf -o blade.pdf' % pstats_file)
+                   ' | dot -T pdf -o blade.pdf' % pstats_file)
     return exit_code[0]
 
 

src/blade/blade_platform.py
@@ -9,14 +9,15 @@
  This is the blade_platform module which deals with the environment
  variable.
 """
-
+from __future__ import absolute_import
+from __future__ import print_function
 
 import os
 import subprocess
 
-import config
-import console
-from blade_util import var_to_list
+from blade import config
+from blade import console
+from blade.blade_util import var_to_list, iteritems
 
 
 class BuildArchitecture(object):
@@ -26,41 +27,41 @@ class BuildArchitecture(object):
     command line.
     """
     _build_architecture = {
-        'i386' : {
-            'alias' : ['x86'],
-            'bits' : '32',
+        'i386': {
+            'alias': ['x86'],
+            'bits': '32',
         },
-        'x86_64' : {
-            'alias' : ['amd64'],
-            'bits' : '64',
-            'models' : {
-                '32' : 'i386',
+        'x86_64': {
+            'alias': ['amd64'],
+            'bits': '64',
+            'models': {
+                '32': 'i386',
             }
         },
-        'arm' : {
-            'alias' : [],
-            'bits' : '32'
+        'arm': {
+            'alias': [],
+            'bits': '32'
         },
-        'aarch64' : {
-            'alias' : ['arm64'],
-            'bits' : '64',
+        'aarch64': {
+            'alias': ['arm64'],
+            'bits': '64',
         },
-        'ppc' : {
-            'alias' : ['powerpc'],
-            'bits' : '32',
+        'ppc': {
+            'alias': ['powerpc'],
+            'bits': '32',
         },
-        'ppc64' : {
-            'alias' : ['powerpc64'],
-            'bits' : '64',
-            'models' : {
-                '32' : 'ppc',
+        'ppc64': {
+            'alias': ['powerpc64'],
+            'bits': '64',
+            'models': {
+                '32': 'ppc',
             }
         },
-        'ppc64le' : {
-            'alias' : ['powerpc64le'],
-            'bits' : '64',
-            'models' : {
-                '32' : 'ppcle',
+        'ppc64le': {
+            'alias': ['powerpc64le'],
+            'bits': '64',
+            'models': {
+                '32': 'ppcle',
             }
         },
     }
@@ -69,7 +70,7 @@ class BuildArchitecture(object):
     def get_canonical_architecture(arch):
         """Get the canonical architecture from the specified arch. """
         canonical_arch = None
-        for k, v in BuildArchitecture._build_architecture.iteritems():
+        for k, v in iteritems(BuildArchitecture._build_architecture):
             if arch == k or arch in v['alias']:
                 canonical_arch = k
                 break
@@ -103,6 +104,7 @@ class BuildArchitecture(object):
 
 class BuildPlatform(object):
     """The build platform class which handles and gets the platform info. """
+
     def __init__(self):
         """Init. """
         self.gcc_version = self._get_gcc_version()
@@ -172,7 +174,7 @@ class BuildPlatform(object):
             include_list.append('%s/include/linux' % java_home)
             return include_list
         returncode, stdout, stderr = BuildPlatform._execute(
-                'java -version', redirect_stderr_to_stdout=True)
+            'java -version', redirect_stderr_to_stdout=True)
         if returncode == 0:
             version_line = stdout.splitlines(True)[0]
             version = version_line.split()[2]
@@ -244,6 +246,7 @@ class CcFlagsManager(object):
     """The CcFlagsManager class.
     This class manages the compile warning flags.
     """
+
     def __init__(self, options, build_dir, gcc_version):
         self.cc = ''
         self.options = options
@@ -261,7 +264,7 @@ class CcFlagsManager(object):
         for flag in var_to_list(flag_list):
             cmd = ('echo "int main() { return 0; }" | '
                    '%s -o %s -c -x %s %s - > /dev/null 2>&1 && rm -f %s' % (
-                   self.cc, obj, language, flag, obj))
+                       self.cc, obj, language, flag, obj))
             if subprocess.call(cmd, shell=True) == 0:
                 supported_flags.append(flag)
             else:
@@ -299,10 +302,10 @@ class CcFlagsManager(object):
             flags_except_warning.append('-DNDEBUG')
 
         flags_except_warning += [
-                '-D_FILE_OFFSET_BITS=64',
-                '-D__STDC_CONSTANT_MACROS',
-                '-D__STDC_FORMAT_MACROS',
-                '-D__STDC_LIMIT_MACROS',
+            '-D_FILE_OFFSET_BITS=64',
+            '-D__STDC_CONSTANT_MACROS',
+            '-D__STDC_FORMAT_MACROS',
+            '-D__STDC_LIMIT_MACROS',
         ]
 
         if getattr(self.options, 'gprof', False):
@@ -320,7 +323,7 @@ class CcFlagsManager(object):
                               '-Wl,--no-whole-archive']
 
         flags_except_warning = self._filter_out_invalid_flags(
-                flags_except_warning)
+            flags_except_warning)
 
         return (flags_except_warning, linkflags)
 

src/blade/blade_util.py
@@ -13,6 +13,14 @@
 
 """
 
+from __future__ import absolute_import
+from __future__ import print_function
+
+try:
+    basestring
+except NameError:
+    basestring = str
+
 import fcntl
 import os
 import json
@@ -22,16 +30,16 @@ import string
 import signal
 import subprocess
 
-import console
-
+from blade import console
 
 try:
     import hashlib as md5
 except ImportError:
     import md5
 
-
 location_re = re.compile(r'\$\(location\s+(\S*:\S+)(\s+\w*)?\)')
+
+PY3 = sys.version_info[0] == 3
 
 
 def md5sum_str(user_str):
@@ -45,9 +53,8 @@ def md5sum_str(user_str):
 
 def md5sum_file(file_name):
     """Calculate md5sum of the file. """
-    f = open(file_name)
-    digest = md5sum_str(f.read())
-    f.close()
+    with open(file_name) as f:
+        digest = md5sum_str(f.read())
     return digest
 
 
@@ -59,7 +66,7 @@ def md5sum(obj):
 def lock_file(filename):
     """lock file. """
     try:
-        fd = os.open(filename, os.O_CREAT|os.O_RDWR)
+        fd = os.open(filename, os.O_CREAT | os.O_RDWR)
         old_fd_flags = fcntl.fcntl(fd, fcntl.F_GETFD)
         fcntl.fcntl(fd, fcntl.F_SETFD, old_fd_flags | fcntl.FD_CLOEXEC)
         fcntl.flock(fd, fcntl.LOCK_EX | fcntl.LOCK_NB)
@@ -131,13 +138,13 @@ def find_blade_root_dir(working_dir=None):
     named BLADE_ROOT.
 
     """
-    blade_root = find_file_bottom_up('BLADE_ROOT', from_dir=working_dir)
+    blade_root = find_file_bottom_up(b'BLADE_ROOT', from_dir=working_dir)
     if not blade_root:
         console.error_exit(
-                "Can't find the file 'BLADE_ROOT' in this or any upper directory.\n"
-                "Blade need this file as a placeholder to locate the root source directory "
-                "(aka the directory where you #include start from).\n"
-                "You should create it manually at the first time.")
+            "Can't find the file 'BLADE_ROOT' in this or any upper directory.\n"
+            "Blade need this file as a placeholder to locate the root source directory "
+            "(aka the directory where you #include start from).\n"
+            "You should create it manually at the first time.")
     return os.path.dirname(blade_root)
 
 
@@ -224,3 +231,15 @@ def cpu_count():
 def regular_variable_name(var):
     """Replace the chars of var that scons doesn't recognize. """
     return var.translate(string.maketrans(',-/.+*', '______'))
+
+
+if PY3:
+    def iteritems(d, **kw):
+        return iter(d.items(**kw))
+else:
+    def iteritems(d, **kw):
+        return d.iteritems(**kw)
+
+
+def exec_(filename, globals, locals):
+    exec(compile(open(filename, "rb").read(), filename, 'exec'), globals, locals)

src/blade/build_attributes.py
@@ -10,13 +10,13 @@ such as bits: 32/64, profile: debug/release.
 
 """
 
-
 # Global target attributes object
 attributes = None
 
 
 class TargetAttributes(object):
     """Build target attributes. """
+
     def __init__(self, options):
         self.options = options
 

src/blade/build_environment.py
@@ -10,18 +10,18 @@
 
 """
 
+from __future__ import absolute_import
+from __future__ import print_function
 
-import glob
-import math
 import os
 import subprocess
-import time
 
-import console
+from blade import console
 
 
 class BuildEnvironment(object):
     """Managers ccache, distcc. """
+
     def __init__(self, blade_root_dir, distcc_host_list=None):
         # ccache
         self.blade_root_dir = blade_root_dir
@@ -83,7 +83,6 @@ class BuildEnvironment(object):
             if version_line and version_line.find('distcc') != -1:
                 console.debug('distcc found')
                 return True
-
 
     def setup_build_cache(self, options):
         if self.ccache_installed:

commit 0d9f89024d6fb821be77c4e75229dc9ddb312533

Fix bug introduced by #534

src/blade/toolchain.py
@@ -134,7 +134,7 @@ def generate_securecc_object_entry(args):
 
 def generate_resource_index(targets, sources, name, path):
     header, source = targets
-    with open(header, 'w') as h, open('w', source) as c:
+    with open(header, 'w') as h, open(source, 'w') as c:
         full_name = blade_util.regular_variable_name(os.path.join(path, name))
         guard_name = 'BLADE_RESOURCE_%s_H_' % full_name.upper()
         index_name = 'RESOURCE_INDEX_%s' % full_name