a4e2b0e4b4619e2a9891955a12eeb88bf26a98ab
chen3feng <chen3feng@gmail.com>
2020-05-13 00:52:01 +0000

    Fix imports

--- src/blade/__init__.py
+++ src/blade/__init__.py
@@ -1,15 +1,19 @@
 """Public API for user rules"""
 
-import os
-import blade.build_manager
+# Default imports for user code
+import config
+import console
 
 
 def current_source_dir():
     """Get current source dir (in which the current BUILD resident) relative to WORKSPACE root"""
+    import blade.build_manager
     return blade.build_manager.instance.get_current_source_path()
 
 
 def current_target_dir():
     """Get corresponding target dir of current source dir, such as build64_release/xxx"""
+    import os
+    import blade.build_manager
     return os.path.join(blade.build_manager.instance.get_build_path(), current_source_dir())
 
