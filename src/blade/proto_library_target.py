db3f3770758fa081f173ab1a981a1ea81e017bcc
chen3feng <chen3feng@gmail.com>
2020-08-11 21:14:46 +0800

    add comments

--- src/blade/proto_library_target.py
+++ src/blade/proto_library_target.py
@@ -63,6 +63,8 @@ class ProtocPlugin(object):
             self.name, self.path, self.name, out)
 
     def __repr__(self):
+        # This object is a member of proto target's data, provide a textual repr here to make
+        # rule_hash reproducable between each build.
         return 'ProtocPlugin(%s)' % self.__dict__
 
 class ProtoLibrary(CcTarget, java_targets.JavaTargetMixIn):
