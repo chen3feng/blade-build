3a17f8f532f51b9f9a8f499a31b2950675c5b0da
chen3feng <chen3feng@gmail.com>
2020-08-18 01:49:05 +0800

    Fix config section order

--- src/blade/config.py
+++ src/blade/config.py
@@ -49,7 +49,7 @@ class BladeConfig(object):
         # __doc__ suffix of items are for items.
         self.configs = {
             'global_config': {
-                '__doc__': 'Global configuration',
+                '__doc__': 'Global Configuration',
                 'build_path_template': 'build${bits}_${profile}',
                 'duplicated_source_action': 'warning',
                 'duplicated_source_action__doc__': "Can be 'warning', 'error', 'none'",
@@ -70,114 +70,8 @@ class BladeConfig(object):
                     'Whether run unrepaired(no changw after previous failure) tests during incremental test',
             },
 
-            'cc_test_config': {
-                'dynamic_link': False,
-                'heap_check': '',
-                'gperftools_libs': [],
-                'gperftools_debug_libs': [],
-                'gtest_libs': [],
-                'gtest_main_libs': [],
-                'pprof_path': '',
-            },
-
-            'cc_binary_config': {
-                'extra_libs': [],
-                'run_lib_paths': [],
-            },
-
-            'distcc_config': {
-                'enabled': False
-            },
-
-            'link_config': {
-                'link_on_tmp': False,
-                'link_jobs': None,
-            },
-
-            'java_config': {
-                'version': '1.8',
-                'source_version': '',
-                'target_version': '',
-                'maven': 'mvn',
-                'maven_central': '',
-                'maven_snapshot_update_policy': 'daily',
-                'maven_snapshot_update_policy__doc__':
-                    'Can be %s' % _MAVEN_SNAPSHOT_UPDATE_POLICY_VALUES,
-                'maven_snapshot_update_interval': 0,
-                'maven_snapshot_update_interval__doc__': 'When policy is interval, in minutes',
-                'warnings': ['-Werror', '-Xlint:all'],
-                'source_encoding': None,
-                'java_home': '',
-                'debug_info_levels': {
-                    'no': ['-g:none'],
-                    'low': ['-g:source'],
-                    'mid': ['-g:source,lines'],
-                    'high': ['-g'],
-                },
-            },
-
-            'java_binary_config': {
-                'one_jar_boot_jar': '',
-            },
-            'java_test_config': {
-                'junit_libs': [],
-                'jacoco_home': '',
-            },
-            'scala_config': {
-                'scala_home': '',
-                'target_platform': '',
-                'warnings': '',
-                'source_encoding': None,
-            },
-            'scala_test_config': {
-                'scalatest_libs': '',
-            },
-            'go_config': {
-                'go': '',
-                'go_home': os.path.expandvars('$HOME/go'),  # GOPATH
-                # enable go module for explicit use
-                'go_module_enabled': os.environ.get("GO111MODULE") == "on",
-                # onetree repository go module doesn't work in repository root
-                'go_module_relpath': os.environ.get("go_module_relpath"),
-            },
-            'thrift_config': {
-                'thrift': 'thrift',
-                'thrift_libs': [],
-                'thrift_incs': [],
-                'thrift_gen_params': 'cpp:include_prefix,pure_enums'
-            },
-
-            'fbthrift_config': {
-                'fbthrift1': 'thrift1',
-                'fbthrift2': 'thrift2',
-                'fbthrift_libs': [],
-                'fbthrift_incs': [],
-            },
-
-            'proto_library_config': {
-                'protoc': 'thirdparty/protobuf/bin/protoc',
-                'protoc_java': '',
-                'protobuf_libs': [],
-                'protobuf_path': '',
-                'protobuf_incs': [],
-                'protobuf_java_incs': [],
-                'protobuf_php_path': '',
-                'protoc_php_plugin': '',
-                'protobuf_java_libs': [],
-                'protoc_go_plugin': '',
-                'protoc_go_subplugins': [],
-                # All the generated go source files will be placed
-                # into $GOPATH/src/protobuf_go_path
-                'protobuf_go_path': '',
-                'protobuf_python_libs': [],
-                'protoc_direct_dependencies': False,
-                'well_known_protos': [],
-            },
-
-            'protoc_plugin_config': {
-            },
-
             'cc_config': {
+                '__doc__': 'C/C++ Configuration',
                 'extra_incs': [],
                 'cppflags': [],
                 'cflags': [],
@@ -203,7 +97,9 @@ class BladeConfig(object):
                 'hdr_dep_missing_suppress': {},
                 'hdr_dep_missing_suppress__doc__': 'header deps missing suppress control, see docs for details',
             },
+
             'cc_library_config': {
+                '__doc__': 'C/C++ Library Configuration',
                 'prebuilt_libpath_pattern': 'lib${bits}',
                 'generate_dynamic': None,
                 # Options passed to ar/ranlib to control how
@@ -213,7 +109,132 @@ class BladeConfig(object):
                 'ranlibflags': [],
                 'hdrs_missing_severity': 'error',
                 'hdrs_missing_suppress': set(),
-            }
+            },
+
+            'cc_binary_config': {
+                '__doc__': 'C/C++ Executable Configuration',
+                'extra_libs': [],
+                'run_lib_paths': [],
+            },
+
+            'cc_test_config': {
+                '__doc__': 'C/C++ Test Configuration',
+                'dynamic_link': False,
+                'heap_check': '',
+                'gperftools_libs': [],
+                'gperftools_debug_libs': [],
+                'gtest_libs': [],
+                'gtest_main_libs': [],
+                'pprof_path': '',
+            },
+
+            'distcc_config': {
+                'enabled': False
+            },
+
+            'link_config': {
+                '__doc__': 'Linking Configuration',
+                'link_on_tmp': False,
+                'link_jobs': None,
+            },
+
+            'java_config': {
+                '__doc__': 'Java Configuration',
+                'version': '1.8',
+                'source_version': '',
+                'target_version': '',
+                'maven': 'mvn',
+                'maven_central': '',
+                'maven_snapshot_update_policy': 'daily',
+                'maven_snapshot_update_policy__doc__':
+                    'Can be %s' % _MAVEN_SNAPSHOT_UPDATE_POLICY_VALUES,
+                'maven_snapshot_update_interval': 0,
+                'maven_snapshot_update_interval__doc__': 'When policy is interval, in minutes',
+                'warnings': ['-Werror', '-Xlint:all'],
+                'source_encoding': None,
+                'java_home': '',
+                'debug_info_levels': {
+                    'no': ['-g:none'],
+                    'low': ['-g:source'],
+                    'mid': ['-g:source,lines'],
+                    'high': ['-g'],
+                },
+            },
+
+            'java_binary_config': {
+                '__doc__': 'Java Executable Configuration',
+                'one_jar_boot_jar': '',
+            },
+
+            'java_test_config': {
+                '__doc__': 'Java Test Configuration',
+                'junit_libs': [],
+                'jacoco_home': '',
+            },
+
+            'scala_config': {
+                '__doc__': 'Scala Configuration',
+                'scala_home': '',
+                'target_platform': '',
+                'warnings': '',
+                'source_encoding': None,
+            },
+
+            'scala_test_config': {
+                '__doc__': 'Scala Test Configuration',
+                'scalatest_libs': '',
+            },
+
+            'go_config': {
+                '__doc__': 'Golang Configuration',
+                'go': '',
+                'go_home': os.path.expandvars('$HOME/go'),  # GOPATH
+                # enable go module for explicit use
+                'go_module_enabled': os.environ.get("GO111MODULE") == "on",
+                # onetree repository go module doesn't work in repository root
+                'go_module_relpath': os.environ.get("go_module_relpath"),
+            },
+
+            'proto_library_config': {
+                '__doc__': 'Protobuf Configuration',
+                'protoc': 'thirdparty/protobuf/bin/protoc',
+                'protoc_java': '',
+                'protobuf_libs': [],
+                'protobuf_path': '',
+                'protobuf_incs': [],
+                'protobuf_java_incs': [],
+                'protobuf_php_path': '',
+                'protoc_php_plugin': '',
+                'protobuf_java_libs': [],
+                'protoc_go_plugin': '',
+                'protoc_go_subplugins': [],
+                # All the generated go source files will be placed
+                # into $GOPATH/src/protobuf_go_path
+                'protobuf_go_path': '',
+                'protobuf_python_libs': [],
+                'protoc_direct_dependencies': False,
+                'well_known_protos': [],
+            },
+
+            'protoc_plugin_config': {
+                '__doc__': 'Protobuf Plugin Configuration',
+            },
+
+            'thrift_config': {
+                '__doc__': 'Thrift Configuration',
+                'thrift': 'thrift',
+                'thrift_libs': [],
+                'thrift_incs': [],
+                'thrift_gen_params': 'cpp:include_prefix,pure_enums'
+            },
+
+            'fbthrift_config': {
+                '__doc__': 'Facebook Thrift Configuration',
+                'fbthrift1': 'thrift1',
+                'fbthrift2': 'thrift2',
+                'fbthrift_libs': [],
+                'fbthrift_incs': [],
+            },
         }
 
     def info(self, msg):
@@ -361,12 +382,25 @@ def _check_test_related_envs(kwargs):
                 '"global_config.test_related_envs": Invalid env name or regex "%s", %s' % (name, e))
 
 
+_DUPLICATED_SOURCE_ACTION_VALUES = set(['warning', 'error', 'none', None])
+
+
 @config_rule
 def config_items(**kwargs):
     """Used in config functions for config file, to construct a appended
     items dict, and then make syntax more pretty
     """
     return kwargs
+
+
+@config_rule
+def global_config(append=None, **kwargs):
+    """global_config section. """
+    _check_kwarg_enum_value(kwargs, 'duplicated_source_action', _DUPLICATED_SOURCE_ACTION_VALUES)
+    debug_info_levels = _blade_config.get_section('cc_config')['debug_info_levels'].keys()
+    _check_kwarg_enum_value(kwargs, 'debug_info_level', debug_info_levels)
+    _check_test_related_envs(kwargs)
+    _blade_config.update_config('global_config', append, kwargs)
 
 
 @config_rule
@@ -388,17 +422,16 @@ def cc_library_config(append=None, **kwargs):
     _blade_config.update_config('cc_library_config', append, kwargs)
 
 
-_DUPLICATED_SOURCE_ACTION_VALUES = set(['warning', 'error', 'none', None])
-
-
 @config_rule
-def global_config(append=None, **kwargs):
-    """global_config section. """
-    _check_kwarg_enum_value(kwargs, 'duplicated_source_action', _DUPLICATED_SOURCE_ACTION_VALUES)
-    debug_info_levels = _blade_config.get_section('cc_config')['debug_info_levels'].keys()
-    _check_kwarg_enum_value(kwargs, 'debug_info_level', debug_info_levels)
-    _check_test_related_envs(kwargs)
-    _blade_config.update_config('global_config', append, kwargs)
+def cc_config(append=None, **kwargs):
+    """extra cc config, like extra cpp include path splited by space. """
+    _check_kwarg_enum_value(kwargs, 'hdr_dep_missing_severity', ['debug', 'info', 'warning', 'error'])
+    if 'extra_incs' in kwargs:
+        extra_incs = kwargs['extra_incs']
+        if isinstance(extra_incs, str) and ' ' in extra_incs:
+            _blade_config.warning('"cc_config.extra_incs" has been changed to list')
+            kwargs['extra_incs'] = extra_incs.split()
+    _blade_config.update_config('cc_config', append, kwargs)
 
 
 @config_rule
@@ -488,14 +521,3 @@ def fbthrift_library_config(append=None, **kwargs):
     """fbthrift config. """
     _blade_config.update_config('fbthrift_config', append, kwargs)
 
-
-@config_rule
-def cc_config(append=None, **kwargs):
-    """extra cc config, like extra cpp include path splited by space. """
-    _check_kwarg_enum_value(kwargs, 'hdr_dep_missing_severity', ['debug', 'info', 'warning', 'error'])
-    if 'extra_incs' in kwargs:
-        extra_incs = kwargs['extra_incs']
-        if isinstance(extra_incs, str) and ' ' in extra_incs:
-            _blade_config.warning('"cc_config.extra_incs" has been changed to list')
-            kwargs['extra_incs'] = extra_incs.split()
-    _blade_config.update_config('cc_config', append, kwargs)
