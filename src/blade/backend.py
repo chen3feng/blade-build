273bd547d75daf60b55803002b0381cccd18bd1c
wintonwang <wintonwang@tencent.com>
2021-05-18 20:32:04 +0800

    fix: exceed maximal length of bash when linking

--- src/blade/backend.py
+++ src/blade/backend.py
@@ -301,13 +301,20 @@ class _NinjaFileHeaderGenerator(object):
         else:
             pool = None
 
-        link_args = '-o ${out} ${intrinsic_linkflags} ${linkflags} ${target_linkflags} ${in} ${extra_linkflags}'
+        # link might have a lot of object files exceeding maximal length of a bash command line.
+        # using response file can avoid this situation.
+        # refer to: https://ninja-build.org/manual.html
+        link_args = '-o ${out} ${intrinsic_linkflags} ${linkflags} ${target_linkflags} @${out}.rspfile ${extra_linkflags}'
         self.generate_rule(name='link',
-                           command=ld + ' ' + link_args,
+                           command= 'touch ${out}.rspfile &&' + ' ' + ld + ' ' + link_args,
+                           rspfile = '${out}.rspfile',
+                           rspfile_content='${in}',
                            description='LINK BINARY ${out}',
                            pool=pool)
         self.generate_rule(name='solink',
-                           command=ld + ' -shared ' + link_args,
+                           command='touch ${out}.rspfile &&' + ' ' + ld + ' -shared ' + link_args,
+                           rspfile = '${out}.rspfile',
+                           rspfile_content='${in}',
                            description='LINK SHARED ${out}',
                            pool=pool)
 
