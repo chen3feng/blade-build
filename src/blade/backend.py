5d27b614639f16579ea22b7b649d962ba5c54b18
jefferydeng <jefferydeng@tencent.com>
2022-12-01 23:13:47 +0800

    fix some description

--- src/blade/backend.py
+++ src/blade/backend.py
@@ -35,8 +35,8 @@ from blade import util
 # The inclusion information is writes to stdout, other normal diagnostic message is write to stderr.
 #
 # NOTE the `$$` is required by ninja. and the `Multiple...` followed by multi lines of filepath are useless part.
-# `多个防止重包含可能对其有用：` is the same as `Multiple...` in chinese.
-# After `Multiple...`, there are still some useful messages, like gcc warnings or cuda error.
+# `多个防止重包含可能对其有用：` is the same as `Multiple...` in Chinese.
+# After `Multiple...`, maybe there are still some useful messages, such as cuda error.
 _INCLUSION_STACK_SPLITTER = (r"awk '"
     r"""/Multiple include guards may be useful for:|多个防止重包含可能对其有用：/ {stop=1} """  # Can't exit here otherwise SIGPIPE maybe occurs.
     r"""/^\.+ [^\/]/ { print $$0} """  # Non absolute path
