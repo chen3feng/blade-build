cf8ad7cb814f5cd983b28a517d44c68592108fbf
jefferydeng <jefferydeng@tencent.com>
2023-08-22 19:42:18 +0800

    fix python ci config and compatibility to python3

--- src/blade/cc_targets.py
+++ src/blade/cc_targets.py
@@ -17,7 +17,11 @@ from __future__ import print_function
 import os
 from string import Template
 
-from distutils.version import LooseVersion
+try:
+    from packaging.version import parse as version_parse
+except ImportError:
+    from distutils.version import LooseVersion as version_parse
+
 
 from blade import build_manager
 from blade import build_rules
@@ -1323,7 +1327,7 @@ class CcBinary(CcTarget):
     def _generate_cc_binary_link_flags(self, dynamic_link):
         linkflags = []
         toolchain = self.blade.get_build_toolchain()
-        if not dynamic_link and toolchain.cc_is('gcc') and LooseVersion(toolchain.get_cc_version()) > LooseVersion('4.5'):
+        if not dynamic_link and toolchain.cc_is('gcc') and version_parse(toolchain.get_cc_version()) > version_parse('4.5'):
             linkflags += ['-static-libgcc', '-static-libstdc++']
         if self.attr.get('export_dynamic'):
             linkflags.append('-rdynamic')
