c189f50a37fc81a91971a20b45b6d23d1bb25154
phongchen <phongchen@tencent.com>
2023-10-04 01:17:50 +0800

    Add override

--- src/blade/util.py
+++ src/blade/util.py
@@ -21,6 +21,7 @@ import hashlib
 import inspect
 import json
 import os
+import re
 import signal
 import string
 import subprocess
@@ -379,3 +380,50 @@ def which(cmd):
     if returncode != 0:
         return None
     return stdout.strip()
+
+
+def override(method):
+    """
+    Check method override.
+    https://stackoverflow.com/a/14631397
+    """
+
+    stack = inspect.stack()
+    base_classes = _get_bass_classes(stack)
+
+    # TODO: check signature
+    # sig = inspect.signature(method)
+    error = "methid '%s' doesn't override any base class method" % method.__name__
+    assert( any( hasattr(cls, method.__name__) for cls in base_classes ) ), error
+    return method
+
+
+def _get_bass_classes(stack):
+    base_classes = re.search(r'class.+\((.+)\)\s*\:', stack[2][4][0]).group(1)
+
+    # handle multiple inheritance
+    base_classes = [s.strip() for s in base_classes.split(',')]
+    if not base_classes:
+        raise ValueError('override decorator: unable to determine base class')
+
+    # stack[0]=override, stack[1]=inside class def'n, stack[2]=outside class def'n
+    derived_class_locals = stack[2][0].f_locals
+
+    # replace each class name in base_classes with the actual class type
+    for i, base_class in enumerate(base_classes):
+
+        if '.' not in base_class:
+            base_classes[i] = derived_class_locals[base_class]
+        else:
+            components = base_class.split('.')
+
+            # obj is either a module or a class
+            obj = derived_class_locals[components[0]]
+
+            for c in components[1:]:
+                assert(inspect.ismodule(obj) or inspect.isclass(obj))
+                obj = getattr(obj, c)
+
+            base_classes[i] = obj
+
+    return base_classes
